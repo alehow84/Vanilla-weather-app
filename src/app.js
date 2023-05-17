d14052328762cc4d3bb7d702630996b5ea4edc86
alehow84 <131718604+alehow84@users.noreply.github.com>
2023-05-17 20:26:40 +0100

    created initial function to amend weather stats

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-alert("hello");
+//function to amend weather stats
+function showWeather(response) {
+    document.querySelector("#city").innerHTML = response.data.city;
+    document.querySelector("#current-temp").innerHTML = Math.round(response.data.temperature.current);
+    document.querySelector("#current-description").innerHTML = response.data.condition.description;
+    document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+    document.querySelector("#humidity").innerHTML = response.data.temperature.humidity;
+}
+
+
+let api = "aa56f014o9bf10caa03ebda1c6dfte85";
+let searchCity = "Tokyo";
+let url = `https://api.shecodes.io/weather/v1/current?query=${searchCity}&key=${api}&units=metric`;
+axios.get(url).then(showWeather);
